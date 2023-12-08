d34784b8a507b8aeb68fae8f3fc31a9234073b97
Ben Sonley <ben.sonley@deptagency.com>
2023-12-08 11:39:27 +0100

    feat: add a table with indicator data for chart

--- src/utils/map-data.ts
+++ src/utils/map-data.ts
@@ -7,6 +7,10 @@ export interface PriceDataRaw {
 		low: number;
 		close: number;
 		volume: number;
+		atr: number;
+		atr_pct: number;
+		sell_stop_loss: number;
+		buy_stop_loss: number;
 	};
 }
 
@@ -17,13 +21,14 @@ export interface PriceDataItem {
 	low: number;
 	close: number;
 }
-export interface VolumeDataItem {
+
+export interface StudyDataItem {
 	time: Time;
 	value: number;
 }
 
 export type PriceData = CandlestickData<Time>[];
-export type VolumeData = VolumeDataItem[];
+export type StudyData = StudyDataItem[];
 
 export const mapPriceData: (data: PriceDataRaw) => PriceData = (data) =>
 	Object.entries(data).map(([time, { open, high, low, close }]) => ({
@@ -34,9 +39,33 @@ export const mapPriceData: (data: PriceDataRaw) => PriceData = (data) =>
 		close,
 	}));
 
-export const mapVolumeData: (data: PriceDataRaw) => VolumeData = (data) =>
+export const mapVolumeData: (data: PriceDataRaw) => StudyData = (data) =>
 	Object.entries(data).map(([time, { open, close, volume }]) => ({
 		time: (parseInt(time) / 1000) as Time,
 		value: volume,
 		color: close >= open ? "#26a69a" : "#ef5350",
+	}));
+
+export const mapAtrData: (data: PriceDataRaw) => StudyData = (data) =>
+	Object.entries(data).map(([time, { atr }]) => ({
+		time: (parseInt(time) / 1000) as Time,
+		value: atr,
+	}));
+
+export const mapAtrPctData: (data: PriceDataRaw) => StudyData = (data) =>
+	Object.entries(data).map(([time, { atr_pct }]) => ({
+		time: (parseInt(time) / 1000) as Time,
+		value: atr_pct,
+	}));
+
+export const mapSellStopLossData: (data: PriceDataRaw) => StudyData = (data) =>
+	Object.entries(data).map(([time, { sell_stop_loss }]) => ({
+		time: (parseInt(time) / 1000) as Time,
+		value: sell_stop_loss,
+	}));
+
+export const mapBuyStopLossData: (data: PriceDataRaw) => StudyData = (data) =>
+	Object.entries(data).map(([time, { buy_stop_loss }]) => ({
+		time: (parseInt(time) / 1000) as Time,
+		value: buy_stop_loss,
 	}));
