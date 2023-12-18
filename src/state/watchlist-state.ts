ae43ea0c8b715d8eb2c6e3e48ea1359ba197df88
Ben Sonley <ben.sonley@deptagency.com>
2023-12-18 16:03:13 +0100

    chore: add types for Intervals and Weeks

--- src/state/watchlist-state.ts
+++ src/state/watchlist-state.ts
@@ -6,10 +6,13 @@ import {
 	map,
 } from "rxjs";
 
+export type Intervals = "90m" | "1d" | "1wk" | "1mo";
+export type Weeks = "2" | "13" | "52" | "260";
+
 class WatchlistState {
-	private _selectedSymbol$ = new BehaviorSubject("");
-	private _selectedInterval$ = new BehaviorSubject("");
-	private _selectedWeeks$ = new BehaviorSubject("");
+	private _selectedSymbol$ = new BehaviorSubject<string | null>(null);
+	private _selectedInterval$ = new BehaviorSubject<Intervals | null>(null);
+	private _selectedWeeks$ = new BehaviorSubject<Weeks | null>(null);
 
 	selectedSymbol$ = this._selectedSymbol$.pipe(distinctUntilChanged());
 	selectedInterval$ = this._selectedInterval$.pipe(distinctUntilChanged());
@@ -23,10 +26,10 @@ class WatchlistState {
 	updateSymbol(value: string) {
 		this._selectedSymbol$.next(value);
 	}
-	updateInterval(value: string) {
+	updateInterval(value: Intervals) {
 		this._selectedInterval$.next(value);
 	}
-	updateWeeks(value: string) {
+	updateWeeks(value: Weeks) {
 		this._selectedWeeks$.next(value);
 	}
 }
