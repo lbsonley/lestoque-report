348a1f5f5f2c7ff3c6d9e5e1bfc565f2f57fd508
Ben Sonley <ben.sonley@deptagency.com>
2024-01-04 15:02:00 +0100

    feat: implement position sizer

--- src/components/widgets/watchlist/position-sizer.ts
+++ src/components/widgets/watchlist/position-sizer.ts
@@ -2,47 +2,76 @@ import state from "@state/watchlist-state";
 
 const template = document.createElement("template");
 template.innerHTML = `
-<form>
-	<label for="portfolioValue">Portfolio Value</label>
-	<input id="portfolioValue" class="input" type="text" name="portfolioValue" value="" />
-	<label for="ATR">ATR</label>
-	<input disabled id="atr" class="input" type="text" name="atr" value="" />
-	<label for="shares">Shares</label>
-	<input disabled id="shares" class="input"type="text" name="shares" value="" />
+<style>
+.position-sizer {
+	display: flex;
+	flex-flow: column nowrap;
+}
+
+.position-sizer--field {
+	display: flex;
+}
+</style>
+<form class="position-sizer">
+	<div class="position-sizer--field">
+		<label for="portfolioValue">Portfolio Value</label>
+		<input id="portfolioValue" class="input" type="text" name="portfolioValue" value="" autocomplete="off" />
+	</div>
+	<div class="position-sizer--field">
+		<label for="entry">Entry</label>
+		<input id="entry" class="input" type="text" name="entry" value="" autocomplete="off" />
+	</div>
+	<div class="position-sizer--field">
+		<label for="stop">Stop</label>
+		<input id="stop" class="input" type="text" name="stop" value="" autocomplete="off" />
+	</div>
+	<button class="position-sizer--button" type="button" id="calc">Calculate</button>
+	<div class="position-sizer--field">
+		<label for="shares">Shares</label>
+		<input disabled id="shares" class="input"type="text" name="shares" value="" />
+	</div>
 </form>
 `;
 
 class PositionSizer extends HTMLElement {
 	portfolioValueEl: HTMLInputElement | null = null;
-	atrEl: HTMLInputElement | null = null;
+	stopEl: HTMLInputElement | null = null;
+	entryEl: HTMLInputElement | null = null;
 	sharesEl: HTMLInputElement | null = null;
-	atr: number | null = null;
+	calculateEl: HTMLButtonElement | null = null;
 
-	handlePortfolioValueInput(event: Event) {
-		console.log(parseInt((event.target as HTMLInputElement).value, 10));
+	handleCalculatePositionSize() {
+		if (
+			this.portfolioValueEl!.value &&
+			this.stopEl!.value &&
+			this.entryEl!.value
+		) {
+			const portfolioRisk = parseInt(this.portfolioValueEl!.value, 10) * 0.01;
+			const perShareRisk = Math.abs(
+				parseFloat(this.entryEl!.value) - parseFloat(this.stopEl!.value),
+			);
+			const shares = Math.floor(portfolioRisk / perShareRisk);
+			this.sharesEl!.value = `${shares}`;
+		}
 	}
 
 	render() {
 		this.shadowRoot!.appendChild(template.content.cloneNode(true));
 		this.portfolioValueEl = this.shadowRoot!.querySelector("#portfolioValue");
-		this.atrEl = this.shadowRoot!.querySelector("#atr");
+		this.stopEl = this.shadowRoot!.querySelector("#stop");
+		this.entryEl = this.shadowRoot!.querySelector("#entry");
 		this.sharesEl = this.shadowRoot!.querySelector("#shares");
-		console.log(this.portfolioValueEl, this.atrEl, this.sharesEl);
+		this.calculateEl = this.shadowRoot!.querySelector("#calc");
 
-		this.portfolioValueEl!.addEventListener(
-			"input",
-			this.handlePortfolioValueInput,
+		this.calculateEl?.addEventListener(
+			"click",
+			this.handleCalculatePositionSize.bind(this),
 		);
 	}
 
 	connectedCallback() {
 		this.attachShadow({ mode: "open" });
 		this.render();
-
-		state.selectedSymbol$.subscribe((symbol) => {
-			console.log(symbol);
-			// todo fetch daily data for symbol
-		});
 	}
 }
 
