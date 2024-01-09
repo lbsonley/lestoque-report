bf9198916472cd77142526be1f9a3ffe378572b8
Ben Sonley <ben.sonley@deptagency.com>
2024-01-09 10:14:05 +0100

    feat: add profit target and risk-reward to position sizer

--- src/components/widgets/watchlist/position-sizer.ts
+++ src/components/widgets/watchlist/position-sizer.ts
@@ -1,4 +1,4 @@
-import state from "@state/watchlist-state";
+import { round } from "@utils/format";
 
 const template = document.createElement("template");
 template.innerHTML = `
@@ -25,47 +25,76 @@ template.innerHTML = `
 		<label for="stop">Stop</label>
 		<input id="stop" class="input" type="text" name="stop" value="" autocomplete="off" />
 	</div>
+	<div class="position-sizer--field">
+		<label for="target">Target</label>
+		<input id="target" class="input" type="text" name="target" value="" autocomplete="off" />
+	</div>
 	<button class="position-sizer--button" type="button" id="calc">Calculate</button>
 	<div class="position-sizer--field">
 		<label for="shares">Shares</label>
 		<input disabled id="shares" class="input"type="text" name="shares" value="" />
+	</div>
+	<div class="position-sizer--field">
+		<label for="risk-reward">Risk/Reward</label>
+		<input disabled id="risk-reward" class="input"type="text" name="risk-reward" value="" />
 	</div>
 </form>
 `;
 
 class PositionSizer extends HTMLElement {
 	portfolioValueEl: HTMLInputElement | null = null;
+	targetEl: HTMLInputElement | null = null;
 	stopEl: HTMLInputElement | null = null;
 	entryEl: HTMLInputElement | null = null;
 	sharesEl: HTMLInputElement | null = null;
+	riskRewardEl: HTMLInputElement | null = null;
 	calculateEl: HTMLButtonElement | null = null;
 
+	handleCalculateRiskReward() {
+		const risk = Math.abs(
+			parseFloat(this.entryEl!.value) - parseFloat(this.stopEl!.value),
+		);
+		const reward = Math.abs(
+			parseFloat(this.targetEl!.value) - parseFloat(this.entryEl!.value),
+		);
+		const riskReward = round(reward / risk);
+		this.riskRewardEl!.value = `${riskReward}`;
+	}
+
 	handleCalculatePositionSize() {
+		const portfolioRisk = parseInt(this.portfolioValueEl!.value, 10) * 0.01;
+		const perShareRisk = Math.abs(
+			parseFloat(this.entryEl!.value) - parseFloat(this.stopEl!.value),
+		);
+		const shares = Math.floor(portfolioRisk / perShareRisk);
+		this.sharesEl!.value = `${shares}`;
+	}
+
+	handleCalculate() {
 		if (
 			this.portfolioValueEl!.value &&
 			this.stopEl!.value &&
+			this.targetEl!.value &&
 			this.entryEl!.value
 		) {
-			const portfolioRisk = parseInt(this.portfolioValueEl!.value, 10) * 0.01;
-			const perShareRisk = Math.abs(
-				parseFloat(this.entryEl!.value) - parseFloat(this.stopEl!.value),
-			);
-			const shares = Math.floor(portfolioRisk / perShareRisk);
-			this.sharesEl!.value = `${shares}`;
+			this.handleCalculatePositionSize();
+			this.handleCalculateRiskReward();
 		}
 	}
 
 	render() {
 		this.shadowRoot!.appendChild(template.content.cloneNode(true));
 		this.portfolioValueEl = this.shadowRoot!.querySelector("#portfolioValue");
+		this.targetEl = this.shadowRoot!.querySelector("#target");
 		this.stopEl = this.shadowRoot!.querySelector("#stop");
 		this.entryEl = this.shadowRoot!.querySelector("#entry");
 		this.sharesEl = this.shadowRoot!.querySelector("#shares");
+		this.riskRewardEl = this.shadowRoot!.querySelector("#risk-reward");
 		this.calculateEl = this.shadowRoot!.querySelector("#calc");
 
 		this.calculateEl?.addEventListener(
 			"click",
-			this.handleCalculatePositionSize.bind(this),
+			this.handleCalculate.bind(this),
 		);
 	}
 
