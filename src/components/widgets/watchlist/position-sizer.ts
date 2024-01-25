0bd65abec724850354176224a116f117fdbfcc8d
Ben Sonley <ben.sonley@deptagency.com>
2024-01-25 15:43:23 +0100

    feat: add hidden symbol field and $risk field to position sizer

--- src/components/widgets/watchlist/position-sizer.ts
+++ src/components/widgets/watchlist/position-sizer.ts
@@ -13,7 +13,8 @@ template.innerHTML = `
 	grid-template-columns: repeat(2, minmax(0, 1fr));
 }
 </style>
-<form class="position-sizer">
+<form class="position-sizer" id="position-sizer-form">
+<input id="symbol" class="input" type="hidden" name="symbol" value="" autocomplete="off" />
 	<div class="position-sizer--field">
 		<label for="portfolioValue">Portfolio Value</label>
 		<input id="portfolioValue" class="input" type="text" name="portfolioValue" value="" autocomplete="off" />
@@ -33,23 +34,33 @@ template.innerHTML = `
 	<button class="position-sizer--button" type="button" id="calc">Calculate</button>
 	<div class="position-sizer--field">
 		<label for="shares">Shares</label>
-		<input disabled id="shares" class="input"type="text" name="shares" value="" />
+		<input id="shares" class="input"type="text" name="shares" value="" />
 	</div>
 	<div class="position-sizer--field">
 		<label for="risk-reward">Risk/Reward</label>
-		<input disabled id="risk-reward" class="input"type="text" name="risk-reward" value="" />
+		<input id="risk-reward" class="input"type="text" name="risk-reward" value="" />
 	</div>
+	<div class="position-sizer--field">
+		<label for="risk-dollar">$Risk</label>
+		<input id="risk-dollar" class="input"type="text" name="risk-dollar" value="" />
+	</div>
+	<button class="position-sizer--button" type="submit" id="save">Save</button>
 </form>
 `;
 
 class PositionSizer extends HTMLElement {
+	symbolEl: HTMLInputElement | null = null;
 	portfolioValueEl: HTMLInputElement | null = null;
 	targetEl: HTMLInputElement | null = null;
 	stopEl: HTMLInputElement | null = null;
 	entryEl: HTMLInputElement | null = null;
 	sharesEl: HTMLInputElement | null = null;
 	riskRewardEl: HTMLInputElement | null = null;
+	riskDollarEl: HTMLInputElement | null = null;
 	calculateEl: HTMLButtonElement | null = null;
+	formEl: HTMLFormElement | null = null;
+
+	static observedAttributes = ["symbol"];
 
 	handleCalculateRiskReward() {
 		const risk = Math.abs(
@@ -63,12 +74,14 @@ class PositionSizer extends HTMLElement {
 	}
 
 	handleCalculatePositionSize() {
-		const portfolioRisk = parseInt(this.portfolioValueEl!.value, 10) * 0.01;
+		const portfolioRisk =
+			parseInt(this.portfolioValueEl!.value, 10) * 0.01 - 10;
 		const perShareRisk = Math.abs(
 			parseFloat(this.entryEl!.value) - parseFloat(this.stopEl!.value),
 		);
 		const shares = Math.floor(portfolioRisk / perShareRisk);
 		this.sharesEl!.value = `${shares}`;
+		this.riskDollarEl!.value = `${portfolioRisk}`;
 	}
 
 	handleCalculate() {
@@ -83,25 +96,42 @@ class PositionSizer extends HTMLElement {
 		}
 	}
 
+	handleSave(event) {
+		event.preventDefault();
+
+		const formData = new FormData(this.formEl!);
+
+		console.log(Object.fromEntries(formData));
+	}
+
 	render() {
 		this.shadowRoot!.appendChild(template.content.cloneNode(true));
+		this.symbolEl = this.shadowRoot!.querySelector("#symbol");
 		this.portfolioValueEl = this.shadowRoot!.querySelector("#portfolioValue");
 		this.targetEl = this.shadowRoot!.querySelector("#target");
 		this.stopEl = this.shadowRoot!.querySelector("#stop");
 		this.entryEl = this.shadowRoot!.querySelector("#entry");
 		this.sharesEl = this.shadowRoot!.querySelector("#shares");
 		this.riskRewardEl = this.shadowRoot!.querySelector("#risk-reward");
+		this.riskDollarEl = this.shadowRoot!.querySelector("#risk-dollar");
 		this.calculateEl = this.shadowRoot!.querySelector("#calc");
+		this.formEl = this.shadowRoot!.querySelector("#position-sizer-form");
 
 		this.calculateEl?.addEventListener(
 			"click",
 			this.handleCalculate.bind(this),
 		);
+
+		this.formEl?.addEventListener("submit", this.handleSave.bind(this));
 	}
 
 	connectedCallback() {
 		this.attachShadow({ mode: "open" });
 		this.render();
+	}
+
+	attributeChangedCallback(name: "symbol", oldValue: string, newValue: string) {
+		this.symbolEl!.value = newValue;
 	}
 }
 
